66fc57a1377ecf8782887622ae38b8f9f9c6ceeb
miri9810 <ada_hack410@hackademy.mx>
2023-01-28 12:08:44 -0600

    fiveth commit
    Modelo de usuarios

--- codigos/serializers.py
+++ codigos/serializers.py
@@ -2,35 +2,40 @@ from rest_framework import serializers
 from .models import Codigos, Municipios, Estados
 
 
-class CodigoSerializer(serializers.ModelSerializer):
+class EstadoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Estados
         exclude = ('status_delete', )
 
 
+class MunicipioSerialier(serializers.ModelSerializer):
+    class Meta: 
+        model = Municipios
+        fields = ('municipios', 'estados')
+
+        def to_representation(self, instance):
+            response = super().to_representation(instance)
+            response['estado'] = EstadoSerializer(instance.estado.all(), many=True).data
+            return response
+
+
 class MoveImgSerializer(serializers.ModelSerializer):
     class Meta:
         model = Move
-        fields = ('id', 'name', 'description', 'amount', 'move_picture', 'day', 'account', 'concept')
-        #exclude = ('status_delete',)
+        fields = ('codigo', 'colonia', 'municipio', 'estado')
 
     def create(self, validated_data):
-        moves = Move.objects.create(
-            name = validated_data.get('name'),
-            description = validated_data.get('description'),
-            amount = validated_data.get('amount'),
-            move_picture = validated_data.get('move_picture'),
-            day = validated_data.get('day'),
-            #user_id = validated_data.get('user_id')            
+        codigo = Codigos.objects.create(
+            codigo = validated_data.get('codigo'),
+            colonia = validated_data.get('colonia')         
         )
-        moves.account.set(validated_data.get('account'))
-        moves.concept.set(validated_data.get('concept'))
-        moves.save()
-        return moves
+        codigo.municipio.set(validated_data.get('municipio'))
+        codigo.estado.set(validated_data.get('estado'))
+        codigo.save()
+        return codigo
 
     def to_representation(self, instance):
         response = super().to_representation(instance)
-        response['concept'] = ConceptSerializer(instance.concept.all(), many=True).data
-        response['account'] = AccountBalanceSerializer(instance.account.all(), many=True).data
-        #response['user'] = {'id':User.id}
+        response['municipio'] = MunicipioSerializer(instance.municipio.all(), many=True).data
+        response['estado'] = EstadoSerializer(instance.estado.all(), many=True).data
         return response
--- core/settings.py
+++ core/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
 
     # local apps
     'codigos'
@@ -139,3 +140,17 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': False,
+    'UPDATE_LAST_LOGIN': False,
+}

a3e2326b45ebca2cd716842d70033d88f7ecb27e
miri9810 <ada_hack410@hackademy.mx>
2023-01-27 21:32:38 -0600

    third commit

--- codigos/models.py
+++ codigos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Codigo(models.Model):
+    colonia = models.CharField(max_length=255, verbose_name='colonia')
+    municipio = models.CharField(max_length=255, verbose_name='municipio')
+    estado = models.CharField(max_length=255, verbose_name='estado')
+
+    class Meta:
+        db_table = 'codigos'
--- core/settings.py
+++ core/settings.py
@@ -32,10 +32,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
-]
+    'rest_framework',
 
-LOCAL_APPS = [
     # local apps
     'codigos'
 ]

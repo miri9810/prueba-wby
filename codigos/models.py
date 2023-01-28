b246c9930cf380d757c307ffaab0dd39b58a8fb1
miri9810 <ada_hack410@hackademy.mx>
2023-01-28 10:33:07 -0600

    third commit

--- codigos/models.py
+++ codigos/models.py
@@ -3,9 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Codigo(models.Model):
+    codigo = models.IntegerField()
     colonia = models.CharField(max_length=255, verbose_name='colonia')
-    municipio = models.CharField(max_length=255, verbose_name='municipio')
-    estado = models.CharField(max_length=255, verbose_name='estado')
 
     class Meta:
         db_table = 'codigos'
